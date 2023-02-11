b7156a3737f69991df42228c7c1c3e25f1c21582
CHI-HAO, CHUANG <xd0931021@gmail.com>
2023-02-11 18:14:23 +0800

    Auto Commit

--- main.py
+++ main.py
@@ -11,44 +11,10 @@ from aiohttp import ClientSession
 LOGGER = getLogger("main")
 
 
-async def main():
-    client = ClientSession()
 
-    w = False
-    s = False
-    c = 0
-    while True:
-        try:
-            res = await client.get("http://localhost:8080/face-data")
-            data = await res.json(loads=Json.loads)
-            if len(data) != 0:
-                if not w:
-                    LOGGER.warning(f"Detect People: {len(data)}")
-                c = 0
-                w = True
-            else:
-                c += 1
-
-            if w and c > 25:
-                res = RECORDER.stop_record()
-                if res:
-                    await FILE_QUEUE.put(res)
-                s = False
-                w = False
-
-            if w and not s:
-                RECORDER.start_record()
-                s = True
-            await asleep(0.2)
-        except CancelledError:
-            break
-
-    await client.close()
 
 if __name__ == "__main__":
     set_event_loop_policy(WindowsSelectorEventLoopPolicy())
     logger_init()
     bot = DiscordBot()
     bot.startup()
-
-    run(main())
